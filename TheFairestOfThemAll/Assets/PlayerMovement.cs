eee1e6fd0e87ca626eb8c6f1c9638af1673ae263
Kerstin Äkke <kerstinakke@gmail.com>
2017-12-27 03:07:34 +0200

    project setup

---
+++ TheFairestOfThemAll/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 3f;
+
+	Vector3 playerDirection = Vector3.forward;
+	public Rigidbody rigidBody;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move ();
+	}
+
+	void Move() {
+		playerDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+		rigidBody.MovePosition (transform.position + playerDirection * speed * Time.deltaTime);
+		print (playerDirection);
+	}
+}
