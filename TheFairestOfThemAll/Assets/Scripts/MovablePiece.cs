b5af400b2e75c69c3eac9fda969f912ce327871a
Kerstin Äkke <kerstinakke@gmail.com>
2018-02-20 15:19:56 +0200

    snapping to place on focus

---
+++ TheFairestOfThemAll/Assets/Scripts/MovablePiece.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovablePiece: Movable {
+
+	public Vector3 correctPos;
+	public Vector3 correctRot;
+	public float m_PosError = 2f;
+	public float m_RotError = 5f;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	public bool CheckPlace(){
+		Vector3 currentPos = transform.position;
+		Vector3 currentRot = transform.eulerAngles;
+		print(correctPos - currentPos);
+		if (Mathf.Abs (correctPos.x - currentPos.x) <= m_PosError 
+			&& Mathf.Abs (correctPos.y - currentPos.y) <= m_PosError 
+			&& Mathf.Abs (correctPos.z - currentPos.z)<= m_PosError) {
+			if (Mathf.Abs (correctRot.x - currentRot.x) <= m_RotError 
+				&& Mathf.Abs (correctRot.y - currentRot.y) <= m_RotError
+				&& Mathf.Abs (correctRot.z - currentRot.z) <= m_RotError) {
+				transform.eulerAngles = correctRot;
+				transform.localPosition = correctPos;
+				print ("yay");
+				return true;
+			}
+		}
+		return false;
+	}
+}
--- TheFairestOfThemAll/Assets/Scripts/Puzzle.cs
+++ TheFairestOfThemAll/Assets/Scripts/Puzzle.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class Puzzle : MonoBehaviour {
 
 	public Vector3 correctPlayerPos;
-	public Piece[] movingPieces;
+	public MovablePiece[] movingPieces;
 
 	private bool m_IsFocus = false;
-	private bool m_Checked = false;
+	private float m_PlayerError = 0.2f;
 	public bool m_Solved = false;
 	private GameObject m_Player;
 
@@ -22,26 +22,23 @@ public class Puzzle : MonoBehaviour {
 		if (!m_Solved) {
 			bool focusing = Input.GetKey (KeyCode.F);
 			if (!m_IsFocus && focusing) {
-				m_Checked = false;
-			}
-			m_IsFocus = focusing;
-			if (m_IsFocus && !m_Checked) {
 				Vector3 pos = m_Player.transform.position;
 				print (pos+" "+correctPlayerPos);
-				if (Mathf.Abs (pos.x - correctPlayerPos.x) <= 0.1 && Mathf.Abs (pos.z - correctPlayerPos.z) <= 0.1) {
+				if (Mathf.Abs (pos.x - correctPlayerPos.x) <= 0.2 && Mathf.Abs (pos.z - correctPlayerPos.z) <= 0.2) {
+					m_Player.transform.position = correctPlayerPos;
 					if (AllSet()) {
 						m_Solved = true;
 						gameObject.transform.Find ("whole").gameObject.SetActive (true);
 					}
 				}
-				m_Checked = true;
 			}
+			m_IsFocus = focusing;
 		}
 	}
 
 	private bool AllSet(){
 		for (int i = 0; i < movingPieces.Length; i++) {
-			if (!movingPieces [i].isInPlace)
+			if (!movingPieces [i].CheckPlace())
 				return false;
 		}
 		return true;
