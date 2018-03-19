a227cfb179ec51ebcbdb7dcc4a2073f5cb3b1657
Kerstin Äkke <kerstinakke@gmail.com>
2018-03-19 15:13:50 +0200

    imageHelper script

---
+++ TheFairestOfThemAll/Assets/Scripts/Helpers/ImageHelper.cs
@@ -0,0 +1,145 @@
+﻿/* made by Raimond-Hendrik Tunnel */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+
+public static class ImageHelper
+{
+
+    /// <summary>
+    /// Converts a 24-bit texture to an EmguCV image of type T.
+    /// </summary>
+    public static Image<T, byte> ToImage<T>(Texture2D tex) where T: struct, IColor
+    {
+        return new Image<T, byte>(ToImageData(tex));
+    }
+
+    /// <summary>
+    /// Converts a 32-bit texture to an EmguCV image of type T.
+    /// </summary>
+    public static Image<T, byte> ToImageA<T>(Texture2D tex) where T : struct, IColor
+    {
+        return new Image<T, byte>(ToImageData(tex, 4));
+    }
+
+    /// <summary>
+    /// Converts a 24-bit texture to an EmguCV image of type Bgr.
+    /// </summary>
+    public static Image<Bgr, byte> ToImage(Texture2D tex)
+    {
+        return ToImage<Bgr>(tex);
+    }
+
+    /// <summary>
+    /// Converts a 32-bit texture to an EmguCV image of type Bgra.
+    /// </summary>
+    public static Image<Bgra, byte> ToImageA(Texture2D tex)
+    {
+        return ToImageA<Bgra>(tex);
+    }
+
+    private static byte[,,] ToImageData(Texture2D tex, int depth = 3)
+    {
+        if (tex.mipmapCount > 1)
+        {
+            Debug.LogWarning(tex.name + " - Can not convert to an image a texture with mipmaps!");
+        }
+        if (tex.format != TextureFormat.RGB24 && tex.format != TextureFormat.RGBA32)
+        {
+            Debug.LogWarning(tex.name + " - Can not convert to an image a texture with a format " + tex.format + "! (only RGB24 is supported)");
+            if (tex.alphaIsTransparency)
+            {
+                Debug.LogWarning(tex.name + " -Transparenct not supported!");
+            }
+        }
+
+        var data = new byte[tex.width, tex.height, depth];
+        byte[] dataLin = tex.GetRawTextureData();
+
+        if (dataLin.GetLength(0) != data.Length)
+        {
+            Debug.LogWarning(tex.name + " -Texture (" + dataLin.GetLength(0) + ") and image (" + data.Length + ") byte counts do not match. Conversion failed.");
+            return null;
+        }
+
+        for (int i = 0; i < dataLin.GetLength(0); i += depth)
+        {
+            int x = (i / depth) % tex.width;
+            int y = (i / depth) / tex.width;
+            data[x, y, 0] = dataLin[i + 0];
+            data[x, y, 1] = dataLin[i + 1];
+            data[x, y, 2] = dataLin[i + 2];
+            if (depth > 3)
+            {
+                data[x, y, 3] = dataLin[i + 3];
+            }
+        }
+
+        return data;
+    }
+
+    /// <summary>
+    /// Converts a EmguCV image of type Bgr to a 24-bit texture.
+    /// </summary>
+    public static Texture2D ToTexture(Image<Bgr, byte> img, TextureFormat format = TextureFormat.RGB24, bool hasMipmaps = false)
+    {
+        if (img == null)
+        {
+            Debug.LogWarning("Can not convert 'null' to a texture!");
+            return null;
+        };
+
+        Texture2D texCopy = new Texture2D(img.Data.GetLength(0), img.Data.GetLength(1), format, hasMipmaps);
+        texCopy.LoadRawTextureData(ToTextureData(img.Data));
+
+        return texCopy;
+    }
+
+    /// <summary>
+    /// Converts a EmguCV image of type Bgr to a 32-bit texture.
+    /// </summary>
+    public static Texture2D ToTexture(Image<Bgra, byte> img, TextureFormat format = TextureFormat.RGBA32, bool hasMipmaps = false)
+    {
+        if (img == null)
+        {
+            Debug.LogWarning("Can not convert 'null' to a texture!");
+            return null;
+        };
+        Debug.Log("aa");
+
+        Texture2D texCopy = new Texture2D(img.Data.GetLength(0), img.Data.GetLength(1), format, hasMipmaps);
+        texCopy.alphaIsTransparency = true;
+        texCopy.LoadRawTextureData(ToTextureData(img.Data));
+
+        return texCopy;
+
+    }
+
+    private static byte[] ToTextureData(byte[,,] data)
+    {
+        byte[] dataLin = new byte[data.GetLength(0) * data.GetLength(1) * data.GetLength(2)];
+
+        for (int x = 0; x < data.GetLength(0); x++)
+        {
+            for (int y = 0; y < data.GetLength(1); y++)
+            {
+                int i = (y * data.GetLength(2)) * data.GetLength(0) + (x * data.GetLength(2));
+                dataLin[i + 0] = data[x, y, 0];
+                dataLin[i + 1] = data[x, y, 1];
+                dataLin[i + 2] = data[x, y, 2];
+                if (data.GetLength(2) > 3)
+                {
+                    dataLin[i + 3] = data[x, y, 3];
+                    Debug.Break();
+                }
+            }
+        }
+
+        return dataLin;
+    }
+
+
+}
--- TheFairestOfThemAll/Assets/Scripts/Puzzle.cs
+++ TheFairestOfThemAll/Assets/Scripts/Puzzle.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
-using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Emgu.CV;
@@ -14,43 +13,47 @@ public class Puzzle : MonoBehaviour {
 	//[SerializeField] private MovablePiece[] movingPieces;
 	[SerializeField] private int level=1;
 
-	private Image<Bgr, Byte> templateImage;
-	private Image<Bgr,Byte> mask;
+	private Image<Bgr, byte> templateImage;
+	private Image<Bgr, byte> mask;
 	private MovableBeacon levelBeacon;
-	private bool WasFocus;
+	private bool wasFocus;
 	private Camera FPCamera;
-	[SerializeField] private bool Solved = false;
+	[SerializeField] private bool solved = false;
 	[SerializeField] private float accuracy = 0.9f;
+	[SerializeField] Texture2D templateImageTexture;
+	[SerializeField] Texture2D maskTexture;
 
 	// Use this for initialization
 	void Start () {
 		levelBeacon = GameObject.FindGameObjectWithTag ("Beacon").GetComponent<MovableBeacon>();
-		WasFocus = false;
+		wasFocus = false;
 		FPCamera = GameObject.FindGameObjectWithTag ("Player").GetComponentInChildren<Camera> ();
-		templateImage = new Image<Bgr, Byte> ("Assets/Rendering/textures/puzzle"+level+"/solution.png");
-		mask = new Image<Bgr, Byte> ("Assets/Rendering/textures/puzzle" + level + "/mask.png");
+		templateImage = ImageHelper.ToImage(templateImageTexture);
+		print (templateImage.Size);
+		mask = ImageHelper.ToImage(maskTexture);
+		print (mask.Size);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (!Solved) {
+		if (!solved) {
 			if (!levelBeacon.GetActive ())
 				return;
 			bool focus = Input.GetKey (KeyCode.F);
-			if (focus && !WasFocus) { //do once after f is pressed	
+			if (focus && !wasFocus) { //do once after f is pressed	
 				levelBeacon.SetCameraActive (true);
 				StartCoroutine(levelBeacon.TakePicture (this));
-			} else if (!focus && WasFocus) {//do once after release
+			} else if (!focus && wasFocus) {//do once after release
 				levelBeacon.SetCameraActive(false);
 				FPCamera.enabled=true;
 			}
-			WasFocus = focus;
+			wasFocus = focus;
 		}
 	}
 
-	public void IsCorrect(){
+	public void CheckCorrect(Texture2D sourceTexture){
 		FPCamera.enabled = false;
-		Image<Bgr,  byte> sourceImage = new Image<Bgr, byte> ("Assets/Rendering/textures/test.png");
+		Image<Bgr,  byte> sourceImage = ImageHelper.ToImage(sourceTexture);
 		Image<Gray, float> result = new Image<Gray, float>(257-templateImage.Width, 257-templateImage.Height);
 		Emgu.CV.CvInvoke.MatchTemplate(sourceImage, templateImage, result, TemplateMatchingType.CcorrNormed, mask);
 		double[] minValues, maxValues;
@@ -58,7 +61,7 @@ public class Puzzle : MonoBehaviour {
 		result.MinMax (out minValues, out maxValues, out minLocations, out maxLocations);
 		print (maxValues[0]+" "+maxLocations[0]);
 		if (maxValues [0] > accuracy) {
-			Solved = true;
+			solved = true;
 			gameObject.transform.Find ("whole").gameObject.SetActive (true);
 			Invoke ("NextLevel", 3f);
 		}
